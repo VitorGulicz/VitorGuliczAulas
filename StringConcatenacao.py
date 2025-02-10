60381b1a7cf416442c7807a4072ce616a8e54576
VitorGulicz <vdmgulicz@gmail.com>
2025-02-10 20:00:56 -0300

    Atividade até o string Texto

---
+++ StringConcatenacao.py
@@ -0,0 +1,4 @@
+primeiro_nome="vitor"
+sobrenome="gulicz"
+nome_completo=primeiro_nome+ " "+sobrenome
+print("Olá, "+ nome_completo.title()+ "!")
---
+++ StringTexto.py
@@ -0,0 +1,7 @@
+email="VitorGulicz@gmail.com"
+
+print("\nPegando posição ZERO no email atribuido:")
+print(email[0])
+
+print("\nPegando da posição ZERO até o onze do email atribuido:")
+print(email[0:11])
