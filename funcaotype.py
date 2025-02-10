58dfd38721b40c95b62b405ea9e248d2e1d7f5af
VitorGulicz <vdmgulicz@gmail.com>
2025-02-10 19:32:13 -0300

    Função type
    Para explicar classes

---
+++ funcaotype.py
@@ -0,0 +1,10 @@
+faturamento=1000
+print("Para variavel faturamento que recebe 1000 o tipo é: ", type(faturamento))
+faturamento=1000.00
+print("Para variavel faturamento que recebe 1000.00 o tipo é: ", type(faturamento))
+faturamento= "1.000"
+print("Para variavel faturamento que recebe '1.000' o tipo é: ", type(faturamento))
+ganha_bonus=True
+print("Para variavel ganha bonus que recebeu 'True' o tipo é: ", type(ganha_bonus))
+
+print(dir(str))
