0f72daf57d2a9af0c61ffefa44439358a5718118
VitorGulicz <vdmgulicz@gmail.com>
2025-02-10 20:58:49 -0300

    até atividade FormatStringFormat

--- StringTexto.py
+++ StringTexto.py
@@ -3,5 +3,9 @@ email="VitorGulicz@gmail.com"
 print("\nPegando posição ZERO no email atribuido:")
 print(email[0])
 
-print("\nPegando da posição ZERO até o onze do email atribuido:")
-print(email[0:11])
+print("\nPegando da posição ZERO até a DOZE do email atribuido:")
+print(email[0:12])
+
+
+print("\nUsando metodo LEN para saber o tamanho da variavel email:")
+print(len(email))
