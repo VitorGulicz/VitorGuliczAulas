771d73f0990d60f5a6fe56a7b20d44d61928253f
VitorGulicz <vdmgulicz@gmail.com>
2025-02-11 19:16:08 -0300

    Exemplo if funcionando

--- ExemploIF.py
+++ ExemploIF.py
@@ -1,3 +1,7 @@
-num=10
-if(num<10):
-print("Numero menor que 10")
+num=int(input("Digite seu numero: "))
+if num>10:
+    print("Numero maior que 10")
+elif num==10:
+    print("Numero igual a 10")
+elif num<10:
+    print("Numero menor que 10")
